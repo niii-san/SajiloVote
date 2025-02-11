5ac453af962e6a0028b64d160e8f3e18d9c56ac6
Nishan Bista <nishanbista525@gmail.com>
2025-02-11 21:26:19 +0545

    fix redirect to login

--- client/src/routes/_layout/_authenticated.tsx
+++ client/src/routes/_layout/_authenticated.tsx
@@ -1,14 +1,15 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_layout/_authenticated")({
-    beforeLoad: async ({ context, location }) => {
+    beforeLoad: async ({ context }) => {
+        console.log("protected route");
         const { isLoggedIn } = context.AuthStore;
 
         if (!isLoggedIn) {
             throw redirect({
                 to: "/login",
                 search: {
-                    redirect: location.href,
+                    redirect: "/",
                 },
             });
         }
