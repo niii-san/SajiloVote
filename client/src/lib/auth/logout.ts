a676fc21a17530527ee50b4097082e1916d66998
Nishan Bista <lw.nishan@gmail.com>
2025-05-09 23:13:22 +0545

    build fixes

--- client/src/lib/auth/logout.ts
+++ client/src/lib/auth/logout.ts
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 
 export const logout = async () => {
     try {
-        const res = await api.get("/api/v1/auth/logout");
+        await api.get("/api/v1/auth/logout");
         window.location.href = "/";
     } catch (error) {
         if (error instanceof AxiosError) {
